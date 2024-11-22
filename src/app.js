17246e399a78575f358f2f9a79791452eb5adc2f
carlos123asd <carlos-medin@hotmail.com>
2024-11-22 01:03:00 +0100

    FIX: route app

--- src/app.js
+++ src/app.js
@@ -1,21 +1,20 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import routeIA from './controllers/controller_IAChatGPT';
+import routeIA from './controllers/controller_IAChatGPT.js';
 
 dotenv.config();
+const app = express();
+const port = 3001;
 
 app.use(cors());
 app.use(express.json());
 
-const app = express();
-const port = 3001;
-
-app.use('analysis', routeIA);
+app.use('/analysis', routeIA);
 
 const startServer = () => {
     try{
-        app.listen(port, () => console.log(`server on port ${port}`))
+        app.listen(port, () => console.log(`server listen on http://localhost:${port}`))
     }catch(error){
         console.error(error);
         process.exit(1)
